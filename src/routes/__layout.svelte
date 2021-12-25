c8aec094fe401b09d9cc66248648a7e9f496fe0d
bmehder <bmehder@gmail.com>
2021-12-25 20:37:42 +0100

    episode 2

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<div class="wrapper">
+  <slot />
+</div>
+
+<style>
+  .wrapper {
+    width: 50vw;
+    margin: 0 auto;
+  }
+  :global(.more-link) {
+    display: none;
+  }
+  :global(img) {
+    max-width: 100%;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch('/api/posts.json')
+    const posts = await res.json()
+    return {
+      props: {
+        posts,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let posts
+</script>
+
+<h1>Headless WerdPress</h1>
+{#each posts as post}
+  <h2>{@html post.title.rendered}</h2>
+  <img src={post.image} alt={post.title.rendered} />
+  <p>{@html post.excerpt.rendered}</p>
+  <a href={`/posts/${post.slug}`}>Read More</a>
+{/each}
